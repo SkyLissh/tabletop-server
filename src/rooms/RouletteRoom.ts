4809480a851d8aa910ede0547d9948968f40dd9d
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2024-07-22 15:33:43 -0600

    :sparkles: Edited room schema
    Edited schema for the roulette room

--- src/rooms/RouletteRoom.ts
+++ src/rooms/RouletteRoom.ts
@@ -2,42 +2,54 @@ import { ArraySchema } from "@colyseus/schema";
 import type { Client } from "colyseus";
 import { logger, Room } from "colyseus";
 
-import type { Rouletteoption } from "@/schemas/RouletteRoomState";
-import { Player, RouletteRoomState } from "@/schemas/RouletteRoomState";
+import { Player, RouletteOption, RouletteRoomState } from "@/schemas/RouletteRoomState";
+
+type PlayerOptions = { name: string; image: string };
+type RouletteOptions = {
+  name: string;
+  options: {
+    name: string;
+    textColor: string | null;
+    backgroundColor: string | null;
+  }[];
+};
 
 export class RouletteRoom extends Room<RouletteRoomState> {
   maxClients: number = 10;
 
-  onCreate(options: { options: Rouletteoption[] }) {
+  onCreate(options: { roulette: RouletteOptions }) {
     const state = new RouletteRoomState();
 
-    if (options.options.length === 0) {
+    if (options.roulette.options.length === 0) {
       this.disconnect();
     }
 
-    state.options = new ArraySchema(...options.options);
-    this.setState(new RouletteRoomState());
+    state.options = new ArraySchema(
+      ...options.roulette.options.map(
+        (option) =>
+          new RouletteOption(option.name, option.textColor, option.backgroundColor)
+      )
+    );
+    state.name = options.roulette.name;
+
+    this.setState(state);
+    this.setPrivate(true);
 
     this.onMessage("spinned", (client, message: { result: string }) => {
-      this.state.result = message.result;
-
-      const index = this.state.players.findIndex(
+      const player = this.state.players.find(
         (player) => player.sessionId === client.sessionId
       );
-
-      if (index === this.state.players.length - 1) {
-        this.state.currentPlayer = this.state.players[0].sessionId;
-        return;
-      }
-      this.state.currentPlayer = this.state.players[index + 1].sessionId;
+      this.broadcast("spinned", { result: message.result, player: player.name });
+      this.state.nextPlayer();
     });
   }
 
-  onJoin(client: Client, options: { name: string; image: string }) {
-    const player = new Player();
-    player.name = options.name;
-    player.image = options.image;
-    player.sessionId = client.sessionId;
+  onJoin(client: Client, options: { player: PlayerOptions }) {
+    const player = new Player(
+      client.sessionId,
+      options.player.name,
+      options.player.image
+    );
     this.state.players.push(player);
 
     if (this.state.players.length === 1) {
@@ -48,10 +60,13 @@ export class RouletteRoom extends Room<RouletteRoomState> {
   }
 
   onLeave(client: Client) {
+    this.state.nextPlayer();
+
     const players = this.state.players.filter(
       (player) => player.sessionId !== client.sessionId
     );
     this.state.players = new ArraySchema(...players);
+    this.allowReconnection(client, 30);
     logger.info(`Client ${client.sessionId} left.`);
   }
 
--- src/schemas/RouletteRoomState.ts
+++ src/schemas/RouletteRoomState.ts
@@ -4,17 +4,43 @@ export class Player extends Schema {
   @type("string") sessionId: string = "";
   @type("string") name: string = "";
   @type("string") image: string = "";
+
+  constructor(sessionId: string, name: string, image: string) {
+    super();
+    this.sessionId = sessionId;
+    this.name = name;
+    this.image = image;
+  }
 }
 
-export class Rouletteoption extends Schema {
+export class RouletteOption extends Schema {
   @type("string") name: string = "";
   @type("string") textColor: string = "";
   @type("string") backgroundColor: string = "";
+
+  constructor(name: string, textColor: string | null, backgroundColor: string | null) {
+    super();
+    this.name = name;
+    this.textColor = textColor;
+    this.backgroundColor = backgroundColor;
+  }
 }
 
 export class RouletteRoomState extends Schema {
   @type([Player]) players = new ArraySchema<Player>();
   @type("string") currentPlayer: string = "";
-  @type("string") result: string = "";
-  @type([Rouletteoption]) options = new ArraySchema<Rouletteoption>();
+  @type("string") name: string = "";
+  @type([RouletteOption]) options = new ArraySchema<RouletteOption>();
+
+  nextPlayer(): void {
+    const index = this.players.findIndex(
+      (player) => player.sessionId === this.currentPlayer
+    );
+
+    if (index === this.players.length - 1) {
+      this.currentPlayer = this.players[0].sessionId;
+      return;
+    }
+    this.currentPlayer = this.players[index + 1].sessionId;
+  }
 }
